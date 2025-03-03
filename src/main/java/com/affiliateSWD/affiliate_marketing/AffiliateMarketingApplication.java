07165c5fa20aeb0f844bcb9e2f757f041d421ae6
Phat <phatbt237@gmail.com>
2025-03-03 23:36:59 +0700

    Initial commit

---
+++ src/main/java/com/affiliateSWD/affiliate_marketing/AffiliateMarketingApplication.java
@@ -0,0 +1,13 @@
+package com.affiliateSWD.affiliate_marketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AffiliateMarketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AffiliateMarketingApplication.class, args);
+	}
+
+}
