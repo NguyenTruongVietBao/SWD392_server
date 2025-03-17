b96b64541a981dfbe058e6fe817f505c48622997
Phat <phatbt237@gmail.com>
2025-03-17 23:49:32 +0700

    corg update 2

--- src/main/java/com/affiliateSWD/affiliate_marketing/config/CORSConfig.java
+++ src/main/java/com/affiliateSWD/affiliate_marketing/config/CORSConfig.java
@@ -17,16 +17,24 @@ public class CORSConfig implements WebMvcConfigurer {
 //                .allowedMethods("*")
 //                .maxAge(1440000);
 //    }
-@Override
-public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-            .allowedOrigins("*")  // Cấu hình để chỉ cho phép từ localhost:5173
-            .allowedHeaders("*")
-            .exposedHeaders("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE")  // Cấu hình các phương thức HTTP mà bạn muốn cho phép
-            .allowCredentials(true)  // Cho phép gửi cookies hoặc authorization headers
-            .maxAge(1440000);  // Thời gian cache cho CORS preflight request
-}
+//@Override
+//public void addCorsMappings(CorsRegistry registry) {
+//    registry.addMapping("/**")
+//            .allowedOrigins("*")  // Cấu hình để chỉ cho phép từ localhost:5173
+//            .allowedHeaders("*")
+//            .exposedHeaders("*")
+//            .allowedMethods("GET", "POST", "PUT", "DELETE")  // Cấu hình các phương thức HTTP mà bạn muốn cho phép
+//            .allowCredentials(true)  // Cho phép gửi cookies hoặc authorization headers
+//            .maxAge(1440000);  // Thời gian cache cho CORS preflight request
+//}
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply CORS policy to all endpoints
+                .allowedOrigins("http://localhost:5173", "https://vercel.com/nt-vbaos-projects/swd-392-client/2VvnWZmhVWiJzbN6R6SP8nr9aHkP") // Allowed origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // HTTP methods
+                .allowedHeaders("Authorization", "Content-Type") // Allowed headers
+                .allowCredentials(true); // Allow credentials (cookies, Authorization headers, etc.)
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
