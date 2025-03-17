9fc6facc9371e21e04d41b1326484a5cd0d72c79
Phat <phatbt237@gmail.com>
2025-03-17 23:16:53 +0700

    corg check

--- src/main/java/com/affiliateSWD/affiliate_marketing/config/CORSConfig.java
+++ src/main/java/com/affiliateSWD/affiliate_marketing/config/CORSConfig.java
@@ -8,15 +8,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CORSConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedHeaders("*")
-                .exposedHeaders("*")
-                .allowedMethods("*")
-                .maxAge(1440000);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedHeaders("*")
+//                .exposedHeaders("*")
+//                .allowedMethods("*")
+//                .maxAge(1440000);
+//    }
+@Override
+public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+            .allowedOrigins("http://localhost:5173")  // Cấu hình để chỉ cho phép từ localhost:5173
+            .allowedHeaders("*")
+            .exposedHeaders("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")  // Cấu hình các phương thức HTTP mà bạn muốn cho phép
+            .allowCredentials(true)  // Cho phép gửi cookies hoặc authorization headers
+            .maxAge(1440000);  // Thời gian cache cho CORS preflight request
+}
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
