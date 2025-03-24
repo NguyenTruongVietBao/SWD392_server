a92a116886445b65eedbde6deb7c550635de0a6a
Van Quoc Thinh <thinhvan.231003@gmail.com>
2025-03-24 14:30:02 +0700

    pull code

--- src/main/java/com/affiliateSWD/affiliate_marketing/respository/PublisherRepository.java
+++ src/main/java/com/affiliateSWD/affiliate_marketing/respository/PublisherRepository.java
@@ -7,10 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 
 public interface PublisherRepository extends JpaRepository<Publisher, Long>
 {
-
+    Optional<Publisher> findByAccountPublisherId(Long accountId);
 }
